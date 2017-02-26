96af56efa99a66c4d9aedd6ed2e7d28ab04c83d9
Iggy <igoririanto@rocketmail.com>
2017-02-25 19:27:57 -0800

    implement user signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get '/hey', to: 'static_pages#hey'
-  get 'signup', to: 'user#new'
+  get 'signup', to: 'users#new'
+  resources :users
 end
---
+++ test/integration/user_signup_test_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class UserSignupTestTest < ActionDispatch::IntegrationTest
+
+  test "invalid user signup" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: {user: {name: '', email: 'user@invalid', password: 'foo', password_confirmation: 'bar'}}
+    end
+    assert_template 'users/new'
+  end
+
+  test "valid user signup" do
+    get signup_path
+    assert_difference 'User.count' do
+      post users_path, params: {user: {name: 'testuser', email: 'testuser@email.com', password: 'foobar', password_confirmation: 'foobar'}}
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
+end
