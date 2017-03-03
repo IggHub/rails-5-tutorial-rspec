ecaff8b1a811ff1c8651c81158f42fb013ad96c3
Iggy <igoririanto@rocketmail.com>
2017-03-03 12:13:36 -0800

    Password reset added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
   has_secure_password
@@ -9,8 +9,8 @@ class User < ApplicationRecord
   validates :password, presence: true, length: {minimum: 5}, allow_nil: true
 
   def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+
     BCrypt::Password.create(string, cost: cost)
   end
 
@@ -41,6 +41,19 @@ class User < ApplicationRecord
    UserMailer.account_activation(self).deliver_now
  end
 
+ def create_reset_digest
+   self.reset_token = User.new_token
+   update_attribute(:reset_digest, User.digest(reset_token))
+   update_attribute(:reset_sent_at, Time.zone.now)
+ end
+
+ def send_password_reset_email
+   UserMailer.password_reset(self).deliver_now
+ end
+
+ def password_reset_expired?
+   reset_sent_at < 2.hours.ago
+ end
  private
 
  def downcase_email
